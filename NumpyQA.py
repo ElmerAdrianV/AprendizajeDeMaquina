a4da1e6c95796a512d12aa1bb49ee04efea7b0a3
elmeradrianv <elmeradrianv@outlook.com>
2022-08-17 17:43:47 -0500

    Clase3/Agregando ejercicio de practica

---
+++ NumpyQA.py
@@ -0,0 +1,15 @@
+'''
+fp = 0
+while fp != 1. :
+    fp = fp + 0.1
+'''
+fp = 0
+while fp != 1. :
+    fp = fp + 0.1
+
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+
+a_mul_b = a*b
